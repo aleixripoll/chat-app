53c8cb010f17635597b011a10f5d69c1cb1646d4
Aleix Ripoll <aleix.ripoll@gmail.com>
2019-10-08 20:48:21 +0200

    fix var

--- public/js/chat.js
+++ public/js/chat.js
@@ -14,7 +14,7 @@ const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true }
 
 const autoscroll = () => {
     // New message element
-    const $newMessage = $messages.lastElementChild
+    const $newMessage = messages.lastElementChild
 
     // Height of the new message
     const newMessageStyles = getComputedStyle($newMessage)
@@ -22,16 +22,16 @@ const autoscroll = () => {
     const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
 
     // Visible height
-    const visibleHeight = $messages.offsetHeight
+    const visibleHeight = messages.offsetHeight
 
     // Height of messages container
-    const containerHeight = $messages.scrollHeight
+    const containerHeight = messages.scrollHeight
 
     // How far have I scrolled?
-    const scrollOffset = $messages.scrollTop + visibleHeight
+    const scrollOffset = messages.scrollTop + visibleHeight
 
     if (containerHeight - newMessageHeight <= scrollOffset) {
-        $messages.scrollTop = $messages.scrollHeight
+        messages.scrollTop = messages.scrollHeight
     }
 }
 
